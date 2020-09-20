be98c139b016f311a9478cfcca263d8c94fad1cb
DVAR <danilka070103@gmail.com>
2020-09-20 07:41:24 +0300

    сделать вид корзины

--- Libr/Pages/Shared/books/ViewBooks.cshtml.cs
+++ Libr/Pages/Shared/books/ViewBooks.cshtml.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Libr.Data.Models;
 using Libr.Data.Repository;
 using Libr.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Web;
+using System.Net.Http;
 
 namespace Libr.Pages.Shared
 {
@@ -13,14 +16,42 @@ namespace Libr.Pages.Shared
     {
         public IEnumerable<book> Books { get; set; }
         private readonly BookRepository bd;
+        private readonly CartRepository CR;
+        [BindProperty]
+        public Cart carts { get; set; }
+        [BindProperty]
+        public book Book { get; set; }
+        public string ip { get; set; }
+        public string login { get; set; }
 
-       public ViewBooksModel(BookRepository context)
+
+        public ViewBooksModel(BookRepository context,CartRepository CartContext)
         {
             bd = context;
+            CR = CartContext;
         }
         public void OnGet()
         {
           Books = bd.GetBooksList();
+            login = HttpContext.User.Identity.Name;
+            ip = HttpContext.Connection.LocalIpAddress.ToString();
+           
+
+        }
+
+
+        public void OnPostOffer(int id)
+        {
+            carts.login = HttpContext.User.Identity.Name;
+            carts.ip = HttpContext.Connection.LocalIpAddress.ToString();
+            Book = bd.objectBook(id);
+            carts.IDBook = Book.ID;
+            carts.NameBook = Book.Name;
+            carts.priceBook = Book.price;
+
+            CR.Buy(carts);
+            CR.save();
+
         }
     }
 }
